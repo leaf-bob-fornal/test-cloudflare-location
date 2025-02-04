e5c4a671e9da49324d4bdf65285bd47a33828657
Bob Fornal <bob.fornal@leafhome.com>
2025-02-04 11:12:50 -0500

    Get from Body

--- src/app/pages/location/location.component.ts
+++ src/app/pages/location/location.component.ts
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './location.component.css'
 })
 export class LocationComponent {
+  timeoutId: any = null;
 
+  city: string = '';
+  state: string = '';
+  zipcode: string = '';
+  ip: string = '';
+
+  constructor() {
+    this.timeoutId = setTimeout(() => {
+      const data = document.querySelector('body');
+      if (data?.hasAttribute('data-ip')) {
+        this.city = data?.getAttribute('data-city') || 'Issue Capturing';
+        this.state = data?.getAttribute('data-state') || 'Issue Capturing';
+        this.zipcode = data?.getAttribute('data-zipcode') || 'Issue Capturing';
+        this.ip = data?.getAttribute('data-ip') || 'Issue Capturing';
+        clearTimeout(this.timeoutId);
+        console.log('timeout cleared');
+      }
+    }, 1000);
+  }
 }
